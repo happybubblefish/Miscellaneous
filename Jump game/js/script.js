4db9e822d20ccf817e4f79004173d4ec5ff4e32a
happybubblefish <congls2007@yahoo.com>
2016-11-15 23:16:16 -0500

    add game level and disable radio features

--- Jump game/js/script.js
+++ Jump game/js/script.js
@@ -5,15 +5,9 @@ var myApp = angular.module("jumpApp", []);
 myApp.controller("jumpCtrl", function($scope, jumpService, $window){
     var count = 5;
     var numbers = [];
-    
-    for(var i = 0; i < 10; i++){
-        var ran = Math.floor(Math.random() * 5);
-        numbers.push(ran);
-    }
-    
+
     $scope.numbers = numbers;
     
-    var result = jumpService.jumpFunc(numbers);
     
     /*$scope.$watch("answer", function(value){
         if(value === result){
@@ -27,22 +21,24 @@ myApp.controller("jumpCtrl", function($scope, jumpService, $window){
     $scope.score = Number(score);
     
     $scope.getAnswer = function(value){
-        if(value === result){
-            console.log("You are right!");
+        if(value === $scope.result){
+            $scope.output = "You are right!";
             $scope.score += 5;
-            
         }else{
             $scope.score -= 2;
-            console.log("Sorry, try it again.");
+            $scope.output = "Sorry, please try another time.";
         }
         
-        $scope.checked = true;
+        $scope.disableCheckedAnswer = true;
     }
     
-    $("#btn-again").click(function(){
+    $scope.playAgain = function(){
         $window.localStorage.setItem("score", $scope.score);
-        $window.location.reload();
-    });
+        $scope.chooseLevel($scope.level);
+        $scope.disableCheckedAnswer = false;
+        //clear the checked radio button
+        $scope.answer = null;
+    };
     
     //use ng-click instead of using jquery click function.
     $scope.reset = function(){
@@ -50,9 +46,33 @@ myApp.controller("jumpCtrl", function($scope, jumpService, $window){
         $scope.score = 0; 
         $window.location.reload();
     };
+    
+    $scope.chooseLevel = function(level){
+        if(level === "easy"){
+            $scope.numbers = jumpService.generateNumbers(5);
+        }else if(level === "normal"){
+            $scope.numbers = jumpService.generateNumbers(10);
+        }else{
+            $scope.numbers = jumpService.generateNumbers(15);
+        }
+        
+        $scope.checkedLevel = true;
+        $scope.result = jumpService.jumpFunc($scope.numbers);
+    }
 });
 
 myApp.service("jumpService", function(){
+    
+    this.generateNumbers = function(length){
+        var numbers = [];
+        for(var i = 0; i < length; i++){
+            var ran = Math.floor(Math.random() * 5);
+            numbers.push(ran);
+        }
+        
+        return numbers;
+    };
+    
     this.jumpFunc = function(numbers){
         var len = numbers.length;
         var max = numbers[0];
