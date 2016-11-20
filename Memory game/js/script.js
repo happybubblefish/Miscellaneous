2a638d877b0aeef2a5c8bb07605f9e28e793fa57
happybubblefish <congls2007@yahoo.com>
2016-11-20 00:25:08 -0500

    refactor code

--- Memory game/js/script.js
+++ Memory game/js/script.js
@@ -17,66 +17,20 @@ app.controller("memoCtrl", function($scope, $compile, memoService, $window){
     var card_board = angular.element(document.querySelector("#memory-cardboard")).append(compiled_card_board);
     
     $scope.msg = null;
+    $scope.flipped_card_id_array = [];
+    $scope.count = 0;
     
-   /* var cards = angular.element(document.querySelector(".card"));*/
-    
-    var flipped_card_id_array = [];
-    var count = 0;
-    $scope.clicked = function(obj){       
+    $scope.clicked = function(obj){
+        var data = memoService.clicked(obj, card_value_array, $scope.flipped_card_id_array);
         
-        //get card id via ng-click event
-        var card_id = obj.target.attributes.id.value;
-        /*var card = angular.element(document.querySelector("#" + card_id + " .default_value"));*/
+        $scope.count += data.count;
+        $scope.flipped_card_id_array = data.flipped_card_id_array;
         
-        //get card value via ng-click event
-        var card_value= obj.target.attributes.data.value;
         
-        var flipped = obj.target.attributes.flipped.value;
-        console.log(flipped);
-        
-        var card = document.getElementById(card_id);
-        /*var filpped = card.getAttribute("flipped");*/
-        /*console.log(filpped);*/
-        
-        var card_span = document.querySelector("#" + card_id + " .default_value");
-        card_span.innerHTML = card_value;
-        card.style.backgroundColor = "#fff";
-        
-        if(flipped == "false"){
-            if(flipped_card_id_array.length == 0){
-                flipped_card_id_array.push(card_id);
-                card.setAttribute("flipped", "true");
-                /*console.log(card.getAttribute("flipped"));*/
-            }else if(flipped_card_id_array.length == 1){
-                var firstCard = document.getElementById(flipped_card_id_array[0]);
-                var firstCard_span = document.querySelector("#" + flipped_card_id_array[0] + " .default_value");
-                var firstCard_value = firstCard.getAttribute("data");
-                
-                card.setAttribute("flipped", "true");
-                
-                if(firstCard_value !== card_value){
-                    setTimeout(function(){
-                        firstCard_span.innerHTML = "?";
-                        firstCard.style.backgroundColor = "deepskyblue";
-                        firstCard.setAttribute("flipped", "false");
-
-                        card_span.innerHTML = "?";
-                        card.style.backgroundColor = "deepskyblue";
-                        card.setAttribute("flipped", "false");
-                    }, 300);
-                }else{
-                    count += 2;
-                    console.log(count);
-                }
-                
-                flipped_card_id_array = [];
-            }
+        if($scope.count === card_value_array.length){
+            $scope.msg = "Congraduations!";
         }
-        
-        if(count === card_value_array.length){
-           $scope.msg = "Congratulations!";
-        }
-    }
+    };
     
     $scope.reset = function(){
       $window.location.reload();  
@@ -102,9 +56,6 @@ app.controller("memoCtrl", function($scope, $compile, memoService, $window){
             var ranIndex1 = Math.floor(Math.random() * (len));
             var ranIndex2 = Math.floor(Math.random() * (len));
             
-            console.log(ranIndex1);
-            console.log(ranIndex2);
-            
             var temp = array[ranIndex1];
             array[ranIndex1] = array[ranIndex2];
             array[ranIndex2] = temp;
@@ -112,4 +63,60 @@ app.controller("memoCtrl", function($scope, $compile, memoService, $window){
         
         return array;
     };
+    
+    this.clicked = function(obj, card_value_array, flipped_card_id_array){
+        var count = 0;
+        var msg = null;
+        
+        //This is JS DOM element
+        var card = obj.target;
+        //get card id via ng-click event
+        var card_id = obj.target.attributes.id.value;
+        /*var card = angular.element(document.querySelector("#" + card_id + " .default_value"));*/
+        
+        //get card value via ng-click event
+        var card_value= obj.target.attributes.data.value;
+        
+        var flipped = obj.target.attributes.flipped.value;
+        
+        /*var card_span = document.querySelector("#" + card_id + " .default_value");*/
+        //to get card element's first child --- span
+        var card_span = card.children[0];
+        card_span.innerHTML = card_value;
+        card.style.backgroundColor = "#fff";
+        
+        if(flipped == "false"){
+            if(flipped_card_id_array.length == 0){
+                flipped_card_id_array.push(card_id);
+                card.setAttribute("flipped", "true");
+            }else if(flipped_card_id_array.length == 1){
+                var firstCard = document.getElementById(flipped_card_id_array[0]);
+                var firstCard_span = document.querySelector("#" + flipped_card_id_array[0] + " .default_value");
+                var firstCard_value = firstCard.getAttribute("data");
+                
+                card.setAttribute("flipped", "true");
+                
+                if(firstCard_value !== card_value){
+                    setTimeout(function(){
+                        firstCard_span.innerHTML = "?";
+                        firstCard.style.backgroundColor = "deepskyblue";
+                        firstCard.setAttribute("flipped", "false");
+
+                        card_span.innerHTML = "?";
+                        card.style.backgroundColor = "deepskyblue";
+                        card.setAttribute("flipped", "false");
+                    }, 300);
+                }else{
+                    count += 2;
+                }
+                
+                flipped_card_id_array = [];
+            }
+        }
+        
+        return {
+            count: count,
+            flipped_card_id_array: flipped_card_id_array
+        };
+    }
 });
